84ca94b419083493962873158ebf0239a9b8fd73
Kiara <frida.shivaa@gmail.com>
2023-10-12 17:08:25 +0200

    add option to form

--- src/main/java/guestbook/GuestbookEntry.java
+++ src/main/java/guestbook/GuestbookEntry.java
@@ -48,6 +48,7 @@ class GuestbookEntry {
 
 		Assert.hasText(name, "Name must not be null or empty!");
 		Assert.hasText(text, "Text must not be null or empty!");
+		Assert.hasText(email, "email must not be null or empty!");
 
 		this.name = name;
 		this.text = text;
